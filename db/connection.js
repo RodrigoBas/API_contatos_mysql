503dd51654dc1bd98a91e41cc71844944937ad5a
RodrigoBas <149122688+RodrigoBas@users.noreply.github.com>
2024-11-11 21:32:41 -0300

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: process.env.HOST || 'localhost',
-    user: process.env.USAURIO || 'usuario',
-    password: process.env.PASS || 'senha',
+    user: process.env.USER || 'usuario',
+    password: process.env.PASSWORD || 'senha',
     database: process.env.DATABASE || 'contatosBD',
     port: process.env.DB_PORT || 3306
 });
